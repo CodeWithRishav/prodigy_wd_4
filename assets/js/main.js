8006b299cf34c892dbb065ea8124c5876fd435c7
codewithrishi <101870417+codewithrishav@users.noreply.github.com>
2022-06-21 16:53:19 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,118 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+	
+
+	/*---------------------------------------------*
+     * Menu Background Change
+     ---------------------------------------------*/
+	
+	var windowWidth = $(window).width();
+    if (windowWidth > 757) {
+
+        
+          
+            $(window).scroll(function () {
+                if ($(this).scrollTop() >200) {
+                    $('.navbar').fadeIn(200);
+                    $('.navbar').addClass('menu-bg');
+
+                } else {
+                    
+                    $('.navbar').removeClass('menu-bg');
+                }
+            });
+        
+    }
+		$('#bs-example-navbar-collapse-1').localScroll();
+		
+	/*---------------------------------------------*
+     * Scroll Up
+     ---------------------------------------------*/	
+		$(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+		});
+		
+		$('.scrollup').click(function(){
+			$("html, body").animate({ scrollTop: 0 },1000);
+			return false;
+		});
+		
+	
+
+
+    /*---------------------------------------------*
+     * STICKY scroll
+     ---------------------------------------------*/
+
+		$.localScroll();
+
+
+
+    /*---------------------------------------------*
+     * Counter 
+     ---------------------------------------------*/
+
+//    $('.statistic-counter').counterUp({
+//        delay: 10,
+//        time: 2000
+//    });
+
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+//        var wow = new WOW({
+//            mobile: false // trigger animations on mobile devices (default is true)
+//        });
+//        wow.init();
+
+
+    /* ---------------------------------------------------------------------
+     Carousel
+     ---------------------------------------------------------------------= */
+
+//    $('.testimonials').owlCarousel({
+//        responsiveClass: true,
+//        autoplay: false,
+//        items: 1,
+//        loop: true,
+//        dots: true,
+//        autoplayHoverPause: true
+//
+//    });
+
+
+    //End
+});
